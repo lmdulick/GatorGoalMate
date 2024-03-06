6fb64de63cfca84914b4da718b2ec6964f6629d4
lmdulick <ldulick17@gmail.com>
2024-03-06 16:43:06 -0500

    update to port # on backend/server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
+// var cors = require('cors')
+
+// const express = require('express')
+// const app = express()
+
+// app.use(cors())
+
+// app.get("/api", (req, res) => {
+//     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
+// })
+
+// app.listen(5000, () => { console.log("Server started on port 5000") })
+
+// // express local host: http://localhost:5000/api
+
 var cors = require('cors')
 
 const express = require('express')
@@ -9,6 +24,8 @@ app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 })
 
-app.listen(5000, () => { console.log("Server started on port 5000") })
-
-// express local host: http://localhost:5000/api
+const server = app.listen(0, () => {
+    console.log(`Server running on port ${server.address().port}`);
+  });
+  
+// express local host: http://localhost:INSERT_PORT_NUMBER/api
